fa17faf9c29cb81532e0f1090253b90f0305bc92
Juan Felipe Garzon Varon <est.juanf.garzon@unimilitar.edu.co>
2026-05-06 21:53:14 -0500

    Primera solución planteada por ChatGPT luego de consultarle

--- src/main/java/Subordinates.java
+++ src/main/java/Subordinates.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 // Node structure for tree
 class Node {
@@ -28,6 +29,9 @@ class Node {
 
 public class Subordinates {
 
+    static List<Integer>[] tree;
+    static int[] subordinates;
+
     // Function to add a child to a node
     static void addChild(Node parent, Node child) {
 
@@ -35,6 +39,18 @@ public class Subordinates {
             return;
 
         parent.children.add(child);
+    }
+
+    static void dfs(int node) {
+
+        subordinates[node] = 0;
+
+        for (int child : tree[node]) {
+
+            dfs(child);
+
+            subordinates[node] += 1 + subordinates[child];
+        }
     }
 
     // Function to print parents of each node
@@ -53,23 +69,34 @@ public class Subordinates {
 
         Scanner sc = new Scanner(System.in);
 
-        // The first input line has an integer n
-        long n = sc.nextLong();
+        int n = sc.nextInt();
 
-        // Relacionado padre - hijo
+        tree = new ArrayList[n + 1];
+        subordinates = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            tree[i] = new ArrayList<>();
+
         Node root = new Node(1);
 
-        for (long i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
 
             Node node = new Node(i);
 
-            long idParent = sc.nextLong();
+            int boss = sc.nextInt();
 
-            addChild(root.get(idParent), node);
+            addChild(root.get(boss), node);
+
+            tree[boss].add(i);
         }
 
         System.out.println("Parents of each node:");
 
         printParents(root, null);
+
+        dfs(1);
+
+        for (int i = 1; i <= n; i++)
+            System.out.print(subordinates[i] + " ");
     }
 }
